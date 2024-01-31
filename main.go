a52423bca98c91e6a86545c438d3e3cdf99d5ae4
Cato Aune <cato@postoffice.no>
2024-01-31 18:43:38 +0100

    Added CreateCardV2

--- main.go
+++ main.go
@@ -20,3 +20,17 @@ func (s *Section) AddWidget() *Widget {
 
 	return &widget
 }
+
+func CreateChatCard(title string) *ChatCard {
+	var chatCard ChatCard
+	var cardsv2 CardsV2
+	var card Card
+	var header CardHeader
+
+	header.Title = title
+	card.Header = &header
+	cardsv2.CardId = "Doorbell"
+	cardsv2.Card = card
+	chatCard.CardsV2 = append(chatCard.CardsV2, cardsv2)
+	return &chatCard
+}
